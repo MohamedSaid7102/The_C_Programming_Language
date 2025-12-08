a5ed6593fdc1d241200cf67f66c16967ed00b733
Shelf <mohamedshelfwork@gmail.com>
2025-12-08 22:03:29 +0200

    Updated the Histogram project

--- Projects/Project 8 - Printing a histogram of the lengths of words in its input/main.c
+++ Projects/Project 8 - Printing a histogram of the lengths of words in its input/main.c
@@ -1,41 +1,8 @@
 #include <stdio.h>
 
-int main() {
-    int c, i, wordLength = 0;
-    int wordLengthCount[25] = {0};  // index 0 = 1-letter words, index 1 = 2-letter words, etc.
-    
-    while ((c = getchar()) != EOF) {
-        if (c == ' ' || c == '\t' || c == '\n') {
-            // Word ended
-            if (wordLength > 0) {
-                if (wordLength <= 25) {
-                    // Store at correct index: wordLength-1
-                    ++wordLengthCount[wordLength-1];
-                }
-                // Could add handling for words longer than 25 here
-                wordLength = 0;
-            }
-        } else {
-            ++wordLength;
-        }
-    }
-    
-    // Don't forget the last word
-    if (wordLength > 0) {
-        if (wordLength <= 25) {
-            ++wordLengthCount[wordLength-1];
-        }
-    }
-    
-    // Print results
-    for (i = 0; i < 25; ++i) {
-        if (wordLengthCount[i] > 0) {
-            printf("%2d-letter words: %2d ", i+1, wordLengthCount[i]);
-        
-        for(int j=0;j<wordLengthCount[i]; ++j) printf("%c", '*');
-        printf(". \n");
-        }
-    }
-    
+/*Exercise 1-13. Write a program to print a histogram of the lengths of words in its input. It is easy to draw the 
+histogram with the bars horizontal; a vertical orientation is more challenging. */
+int main(){
+
     return 0;
 }
