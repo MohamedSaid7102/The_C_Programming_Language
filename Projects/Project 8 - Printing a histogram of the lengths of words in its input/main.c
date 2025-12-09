3ad427f42018c949e82d3d00222399b46ebcb890
Shelf <mohamedshelfwork@gmail.com>
2025-12-09 06:40:59 +0200

    histogram project

--- Projects/Project 8 - Printing a histogram of the lengths of words in its input/main.c
+++ Projects/Project 8 - Printing a histogram of the lengths of words in its input/main.c
@@ -1,17 +1,64 @@
 #include <stdio.h>
 
+#define MAXLETTER 25 /* Suppose the largest word in english is composed of 25 cahrs */
+
+void histogram(int arr[]);
 /*Exercise 1-13. Write a program to print a histogram of the lengths of words in its input. It is easy to draw the 
 histogram with the bars horizontal; a vertical orientation is more challenging. */
 int main(){
-    char str[] = "Hello";
-    
-    printf("String: %s\n", str);
-    printf("Length (including \\0): %zu\n", sizeof(str));
-    
-    // Print each character's ASCII value
-    for (int i = 0; i < sizeof(str); i++) {
-        printf("str[%d] = %d (ASCII)\n", i, str[i]);
+
+    int c,i;
+    int wordLengthArray[MAXLETTER];
+
+    /* Initialize the wordLengthArray with 0s */
+    for (size_t i = 0; i < MAXLETTER; ++i)
+        wordLengthArray[i] = 0;
+
+    /* This is the words length*/
+    i = 0;
+
+    while((c = getchar()) != EOF){
+        ++i;
+        if((c == ' ' || c == '\t' || c == '\n') && i > 0){
+            /* Guared condition to not overflow in case of words > 25 letters */
+            if (i > MAXLETTER) i = MAXLETTER -1;
+
+            ++wordLengthArray[i -1];
+            i = 0; /* Reset i to start counting the next word */
+        }
     }
 
+    /* Considering reaching the end of file without a space termination, we should store this extra word*/
+    if(i>0)
+        ++wordLengthArray[i-1];
+
+    histogram(wordLengthArray);
+
+        
     return 0;
+}
+
+
+/* histogram: prints out the histogram for number of words with a certain characters */
+void histogram(int arr[]){
+
+    /* Print the array list */
+    for (size_t i = 0; i < MAXLETTER; ++i)
+        if(arr[i] !=0)
+            printf("#Words with %2d letters::-> %2d \n",i,arr[i]);
+
+    printf("\n::::::::::::::::::::::::::::::::::::\n");
+    printf("\n:::::: Printing with Stars :::::::::\n");
+    printf("\n::::::::::::::::::::::::::::::::::::\n");
+
+
+    /* Print the array list with stars*/
+    for (size_t i = 0; i < MAXLETTER; ++i){
+        if(arr[i] !=0){
+            printf("#Words with %2d letters::-> ",i);
+            for (size_t j = 0; j < arr[i]; ++j)
+                printf("*") ;
+        printf("\n");
+        }
+    }
 }
