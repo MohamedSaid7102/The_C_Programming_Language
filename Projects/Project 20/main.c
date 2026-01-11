6ad5cb51ec831cbfb04f9e796b19e76f59f8a489
Shelf <mohamedshelfwork@gmail.com>
2026-01-11 11:27:44 +0200

    strequal function

--- Projects/Project 20/main.c
+++ Projects/Project 20/main.c
@@ -3,11 +3,28 @@
 
 void swap(int *a, int *b);
 
+int strlen(char*);
+
+int strlen2(char* );
+
+void strcpy(char*, char*);
+
+int strequal(char* , char* );
+
 int main(){
 
-  int a = 5, b = 10;
-  swap(&a,&b);
-  printf("a: %2d b: %2d", a,b);
+  // int a = 5, b = 10;
+  // swap(&a,&b);
+  // printf("a: %2d b: %2d", a,b);
+  // printf("%p",&a);
+
+  char name[] = "Said";
+  char name2[] = "Said";
+  int nameLeng = strlen2(name);
+  strcpy(name,name2);
+  int result = strequal(name,name2);
+  printf(result ? "Match" : "Missmatch");
+
   
   return 0;
 } 
@@ -17,4 +34,46 @@ void swap(int *a, int *b){
   temp = *a;
   *a = *b;
   *b = temp;
+}
+
+
+/// @brief getting the string length
+/// @param s 
+/// @return string length
+int strlen(char* s){
+  int n;
+  for(n =0; (*s != '\0');s++,n++);
+  return n;
+}
+
+/// @brief another impl of strlen 
+/// @param s 
+/// @return string length
+int strlen2(char* s){
+  char* p = s; /* Pointer p that will point at the start of array s */
+  while(*p!='\0') 
+    ++p;
+  return p -s;
+}
+
+/// @brief copies array t in to array s
+/// @param s 
+/// @param t 
+void strcpy(char *s, char *t){
+  while(*s++ = *t++);
+
+  /*
+    Another version 
+    while((*s++ = *t++) != '\0');
+  */
+}
+
+/// @brief Compare 2 strings
+/// @param s 
+/// @param t 
+/// @return 0 if not match and 1 otherwise 
+int strequal(char* s, char* t){
+  while(*s++ == *t++) 
+    if(*s == '\0') return 1;
+  return 0;
 }
