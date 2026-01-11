7623b50c8761574613b7eecc8f4110812273f692
Shelf <mohamedshelfwork@gmail.com>
2026-01-11 07:04:17 +0200

    swap by reference

--- Projects/Project 20/main.c
+++ Projects/Project 20/main.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 
+
+void swap(int *a, int *b);
+
 int main(){
 
+  int a = 5, b = 10;
+  swap(&a,&b);
+  printf("a: %2d b: %2d", a,b);
   
   return 0;
 } 
+
+void swap(int *a, int *b){
+  int temp;
+  temp = *a;
+  *a = *b;
+  *b = temp;
+}
